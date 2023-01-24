63af6c35a80516d0a03a46a4e09fa5ea57d3b32a
Afolabi <opakunleafolabi@gmail.com>
2023-01-24 22:07:56 +0100

    players component initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { PlayersModule } from './players/players.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        PlayersModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/players/players.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlayersComponent } from './players.component';
+
+
+
+@NgModule({
+    declarations: [
+        PlayersComponent,
+    ],
+    exports: [
+        PlayersComponent,
+    ],
+    imports: [
+        CommonModule,
+    ],
+})
+export class PlayersModule { }
