8a6c45b33899e9f910a8eff0a7d758acb837851b
Afolabi <opakunleafolabi@gmail.com>
2023-01-25 08:18:12 +0100

    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { PlayersModule } from './players/players.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { PlayersModule } from './players/players.module';
   ],
     imports: [
         BrowserModule,
+        HttpClientModule,
         AppRoutingModule,
         PlayersModule,
     ],
--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { PlayersService } from './players.service';
 
 @Component({
   selector: 'app-players',
   templateUrl: './players.component.html',
   styleUrls: ['./players.component.scss']
 })
-export class PlayersComponent {
+export class PlayersComponent implements OnInit {
 
   players$: Observable<any> | undefined;
 
+  constructor(private playersService: PlayersService) {
+  }
 
+  ngOnInit() {
+    this.players$ = this.playersService.getPlayers();
+  }
 }
